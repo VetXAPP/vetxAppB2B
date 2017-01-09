2aacd3a333e3a298c7a3f45828443c28744eb183
arun <web.arun@askpundit.com>
2017-01-09 18:06:49 +0530

    Trello issues has been fixed

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-	//'database': 'mongodb://arun:doodleblue@ds163417.mlab.com:63417/vetdb'
+	'database': 'mongodb://arun:doodleblue@ds163417.mlab.com:63417/vetdb'
 	// 'database': 'mongodb://http://35.164.220.7:3000:27017/vetdb' 
 	// looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-	'database': 'mongodb://127.0.0.1:27017/vetdb' 
+	//'database': 'mongodb://127.0.0.1:27017/vetdb' 
 };
 
 
